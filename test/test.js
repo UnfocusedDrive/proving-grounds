479bafef2906515aea2a73db21e6291a11f86c07
John Yagiz <john.yagiz@gmail.com>
2022-03-26 19:39:59 -0700

    Install Chair Assertion lib for testing

--- test/test.js
+++ test/test.js
@@ -1,5 +1,20 @@
 var assert = require('assert');
+var expect = require("chai").expect;
+
 describe('Array', function () {
+  describe('Filters', function () {
+    it('get unique values', function () {
+      const values = ['a', 'b', 'b'];
+      const getUnique = input => input.filter((value, index, arr) => arr.indexOf(value) === index);
+      const result = getUnique(values);
+      expect(result).to.deep.equal(['a', 'b']);
+    });
+
+    // now get only value more than 1
+  });
+
+
+
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
